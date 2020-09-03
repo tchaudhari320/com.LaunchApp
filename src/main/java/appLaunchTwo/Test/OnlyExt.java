987342dffc0929993576133026618555fbe8b9a3
Vladimir <vladimir.boiko@tut.by>
2020-09-03 10:21:29 +0300

    File created

--- src/main/java/appLaunchTwo/Test/OnlyExt.java
+++ src/main/java/appLaunchTwo/Test/OnlyExt.java
@@ -9,10 +9,6 @@ public class OnlyExt implements FilenameFilter {
     public OnlyExt(String ext) {
         this.ext = "."+ext;
     }
-    public OnlyExt() {
-
-    }
-
     @Override
     public boolean accept(File dir, String name) {
         return name.endsWith(ext);
